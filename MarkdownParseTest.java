da8cfeb25b0c1af4aa6463a9d4eede0b75822ada
Kevin Kim <kevink856@gmail.com>
2022-04-22 08:44:30 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,9 +7,21 @@ import java.util.ArrayList;
 import java.util.List;
 public class MarkdownParseTest {
     @Test
-    public void testReadFile() throws IOException {
+    public void testForTestFile() throws IOException {
         List<String> temp = MarkdownParse.getLinks(Files.readString(Path.of("test-file.md")));
         List<String> expected = List.of("https://something.com", "some-thing.html");
+        assertEquals(expected, temp);
+    }
+    @Test
+    public void testForNewTest() throws IOException {
+        List<String> temp = MarkdownParse.getLinks(Files.readString(Path.of("new-test.md")));
+        List<String> expected = List.of("https://something.com", "some-thing.html", "image.png");
+        assertEquals(expected, temp);
+    }
+    @Test
+    public void testForThirdTestFile() throws IOException {
+        List<String> temp = MarkdownParse.getLinks(Files.readString(Path.of("third-test-file.md")));
+        List<String> expected = List.of("url.md");
         assertEquals(expected, temp);
     }
 }
