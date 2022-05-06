d12adcddc389cd7a9578068f34cf52b49de0c75a
Kyoomin Kim <cs15lsp22aav@ieng6-201.ucsd.edu>
2022-05-06 09:28:16 -0700

    added bash script

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -35,4 +35,10 @@ public class MarkdownParse {
         String temp = "This is a test";
         return toReturn;
     }
+    public static void main(String[] args) throws IOException {
+        Path fileName = Path.of(args[0]);
+        String content = Files.readString(fileName);
+        ArrayList<String> links = getLinks(content);
+        System.out.println(links);
+    }
 }
